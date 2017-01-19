dabb02d62a1f726a118b21fbd71efb22a3047064
Nighteyes604 <nighteyes604@gmail.com>
2017-01-19 05:51:23 -0500

    Updated prevention of pistons generating items from unbroken locks

--- src/main/java/com/meronat/latch/listeners/SpawnEntityListener.java
+++ src/main/java/com/meronat/latch/listeners/SpawnEntityListener.java
@@ -26,29 +26,33 @@
 package com.meronat.latch.listeners;
 
 import com.meronat.latch.Latch;
+import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.cause.entity.spawn.BlockSpawnCause;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnCause;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
 import org.spongepowered.api.event.entity.SpawnEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.cause.Root;
 
 public class SpawnEntityListener {
 
     @Listener
-    public void onSpawnLockItem(SpawnEntityEvent event, @Root SpawnCause sc) {
+    public void onSpawnLockItem(SpawnEntityEvent event, @Root BlockSpawnCause sc) {
         //This is to prevent item drops from being generated when blocks break from things like Pistons
         //Sponge still drops the item even if the blocksnapshot is invalidated/event is cancelled
 
-        //Piston generates a CUSTOM spawn type
-        if( sc.getType() == SpawnTypes.CUSTOM ) {
-            //For each of the entities
-            for(Entity e : event.getEntities()) {
-                //If it's an item and there's a lock there (have to convert item location to block location)
-                if(e.getType() == EntityTypes.ITEM && Latch.getLockManager().getLock(e.getLocation().getExtent().getLocation(e.getLocation().getBlockX(), e.getLocation().getBlockY(), e.getLocation().getBlockZ())).isPresent() ) {
-                    event.setCancelled(true);
-                }
+        //If the blocksnapshot causing the event is not lockable or has no location, no checks needed - return
+        if(!Latch.getLockManager().isLockableBlock(sc.getBlockSnapshot().getState().getType()) || !sc.getBlockSnapshot().getLocation().isPresent()) {
+            return;
+        }
+
+        //Piston used to generate a CUSTOM spawn type, now it's DROPPED_ITEM
+        if( sc.getType() == SpawnTypes.DROPPED_ITEM ) {
+            if(Latch.getLockManager().getLock(sc.getBlockSnapshot().getLocation().get()).isPresent()) {
+                event.setCancelled(true);
             }
         }
     }
