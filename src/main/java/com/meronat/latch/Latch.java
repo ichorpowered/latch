f854e63a6920e0f49d464b870bef598add68d825
Parker Lougheed <parkermeronat@gmail.com>
2018-07-30 17:57:56 -0500

    Update version for 0.5.0 release

--- src/main/java/com/meronat/latch/Latch.java
+++ src/main/java/com/meronat/latch/Latch.java
@@ -43,7 +43,6 @@ import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.event.EventManager;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.GameReloadEvent;
-import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GamePostInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
